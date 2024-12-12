e597ed461f317178fab140042bf875a6f22ec4d8
Ankit Kumar <jiraankit@gmail.com>
2024-12-12 12:49:21 +0530

    Adding CSS for Product Components

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,6 +1,7 @@
+import "./Product.css"
 function Product() {
     return (
-        <div>
+        <div className="Product"> 
             <h1>This is Product Title</h1>
             <h2>This is Product Description</h2>
         </div>
